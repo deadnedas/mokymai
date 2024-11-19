ef3e01ad5d1cdf97b8d6f52f15874b97971d683b
D2JS <d2js@TECHIN.INTERNAL>
2024-11-19 13:34:14 +0400

    fix 14-uzduotis

--- tinklapiu-kurimas/React/6-diena/14-uzduotis/src/App.jsx
+++ tinklapiu-kurimas/React/6-diena/14-uzduotis/src/App.jsx
@@ -8,13 +8,20 @@ function App() {
 
   return (
     <>
-      <LessText
-        text="Focused, hard work is the real key
-      to success. Keep your eyes on the goal,
-and just keep taking the next step
-towards completing it."
-        defaultLength={5}
-      ></LessText>
+      <div className="container">
+        <LessText
+          text="Focused, hard work is the real key
+        to success. Keep your eyes on the goal,
+        and just keep taking the next step
+        towards completing it."
+          defaultLength={5}
+        ></LessText>
+
+        <LessText
+          text="loremLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+          defaultLength={5}
+        ></LessText>
+      </div>
     </>
   );
 }
