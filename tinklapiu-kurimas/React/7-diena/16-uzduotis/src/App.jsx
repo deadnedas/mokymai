8521cd40641490a0cc49b5ed2b5e8b5c37d19ca9
D2JS <d2js@TECHIN.INTERNAL>
2024-11-20 12:40:32 +0400

    finish 16-uzduotis

---
+++ tinklapiu-kurimas/React/7-diena/16-uzduotis/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import PostList from "./components/PostList";
+
+function App() {
+  return (
+    <div className="d-flex justify-content-center">
+      <PostList />
+    </div>
+  );
+}
+
+export default App;
