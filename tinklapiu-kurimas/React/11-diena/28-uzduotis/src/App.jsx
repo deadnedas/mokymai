37eacd4a2148f20884164cb003f554ea36e46010
D2JS <d2js@TECHIN.INTERNAL>
2024-11-27 13:36:22 +0400

    finish 28-uzduotis

---
+++ tinklapiu-kurimas/React/11-diena/28-uzduotis/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import FetchData from "./components/FetchData";
+function App() {
+  return (
+    <>
+      <FetchData></FetchData>
+    </>
+  );
+}
+
+export default App;
---
+++ tinklapiu-kurimas/React/11-diena/28-uzduotis/src/components/FetchData.jsx
@@ -0,0 +1,66 @@
+import { useState, useEffect } from "react";
+
+export default function fetchData() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        const data = await response.json();
+        setData(data);
+        setLoading(false);
+      } catch (error) {
+        setError(error.message);
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
+
+  return (
+    <div className="container">
+      <ul className="row">
+        {data.map((item) => {
+          return (
+            <div className="col-4" key={item.id}>
+              <svg
+                className="bd-placeholder-img rounded-circle"
+                width="140"
+                height="140"
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+                aria-label="Placeholder"
+                preserveAspectRatio="xMidYMid slice"
+                focusable="false"
+              >
+                <rect
+                  width="100%"
+                  height="100%"
+                  fill="var(--bs-secondary-color)"
+                ></rect>
+              </svg>
+
+              <h2 className="fw-normal">{item.name}</h2>
+
+              <p>
+                <a className="btn btn-secondary" href="#">
+                  View details »
+                </a>
+              </p>
+            </div>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
